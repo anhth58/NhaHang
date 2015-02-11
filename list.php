97ec7b8af1c7dcae2608c67916f8e71108ee6fef
anvb-58 <anvb_58@vnu.edu.vn>
2015-02-11 12:48:31 +0700

    version 1.0

---
+++ list.php
@@ -0,0 +1,40 @@
+﻿<html>
+	<title> Xin Cám Ơn </title>
+	<head> </head>
+	<body>
+		<fieldset>
+		<legend> Các món đã chọn </legend>
+		<?php 
+			$fo = fopen("list.txt", "a");
+			if (!empty($_POST["name"])){
+				fwrite($fo, "Khach Hang: ".$_POST["name"]."<br>");
+			}
+			if (!empty($_POST["ga"])){
+				echo "Gà <br>";
+				fwrite($fo, "Gà <br/>");
+			}
+			if (!empty($_POST["vit"])){
+				echo "Vịt<br>";
+				fwrite($fo, "Vịt <br>");
+			}
+			if (!empty($_POST["de"])){
+				echo "Dê<br>";
+				fwrite($fo, "Dê <br>");
+			}
+			if (!empty($_POST["coca"])){
+				echo "Coca x ".$_POST["coca"]."<br>";
+				fwrite($fo, "Coca x ".$_POST["coca"]."<br>");
+			}
+			if (!empty($_POST["pepsi"])){
+				echo "Pepsi x ".$_POST["pepsi"]."<br>";
+				fwrite($fo, "Pepsi x ".$_POST["pepsi"]."<br>");
+			}
+			if (!empty($_POST["sevenup"])){
+				echo "Seven Up x ".$_POST["sevenup"]."<br>";
+				fwrite($fo, "Seven Up x ".$_POST["sevenup"]."<br>");
+			}
+			fwrite($fo, "========<br>");
+		?>
+		</fieldset>
+	</body>
+</html>
---
+++ login.js
@@ -0,0 +1,7 @@
+﻿while (1)
+{
+	tk = prompt("Tên Đăng Nhập");
+	mk = prompt("Mật Khẩu");
+	if (tk == "admin" && mk == "admin") break;
+	alert("Sai tên đăng nhập hoặc mật khẩu");
+}
